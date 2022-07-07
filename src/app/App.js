cf52cfda92ab4a216ffbc23bd66c1dd5e7652d3d
Nick McKinnon <nick89.mck@gmail.com>
2022-07-07 12:10:56 -0400

    Adds rudimentary Weather component to App.

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import '../App.css';
 import {  GoalTable } from '../components/GoalTable';
 import { Quote } from '../features/quotes/Quote';
+import {Weather} from '../features/weather/Weather';
 
 function App() {
   return (
     <div>
+      <Weather />
       <GoalTable />
       <Quote />
     </div>

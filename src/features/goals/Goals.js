e3bbec3c78e5ba36a8ec4ac26f2ba3f3b0a7860b
Nick McKinnon <nick89.mck@gmail.com>
2022-07-07 11:51:59 -0400

    Adds Goal component which shows description and Goals component which creates a list of Goals.

--- src/features/goals/Goals.js
+++ src/features/goals/Goals.js
@@ -1,6 +1,6 @@
+import { Goal } from "./Goal"
 
-
-export const Goals = () => {
+const Goals = () => {
 	const goals = {
 		'1': {
 			id: '1',
@@ -21,15 +21,15 @@ export const Goals = () => {
 		<div>
 			<ul className="goals-list">
 				{ Object.values(goals).map((goal) => (
-					<li className="goal" key={goal.id}>
-						<div className="goal-container">
-							<div className="text-content">
-								<h2>{goal.name}</h2>
-							</div>
-						</div>
-					</li>
+					<Goal
+						id={goal.id}
+						name={goal.name}
+						key={goal.id}
+					/>
 				))}
 			</ul>
 		</div>
 	)
 }
+
+export default Goals

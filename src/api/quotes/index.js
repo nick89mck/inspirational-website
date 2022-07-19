1fd294dd8a1c8d693561c2ec3e77f7c6d9e3da5e
Nick McKinnon <nick89.mck@gmail.com>
2022-07-19 17:51:02 -0400

    adds quote api, default export set to an object that contains a function that returns a quote and author.

--- src/api/quotes/index.js
+++ src/api/quotes/index.js
@@ -4,4 +4,15 @@ export const getQuote = async () => {
 	const response = await fetch(API_URL);
 	const json = await response.json();
 	const quote = json.contents.quotes[0];
+
+	return {
+		quote: quote.quote,
+		author: quote.author
+	}
+};
+
+const quotesApi = {
+	getQuote
 }
+
+export default quotesApi

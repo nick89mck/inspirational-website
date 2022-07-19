d0b8d13372554cfd8da4757243b207252f188bdf
Nick McKinnon <nick89.mck@gmail.com>
2022-07-19 18:16:39 -0400

    Adds quote api functionallity to slice and component.

--- src/features/quotes/Quote.js
+++ src/features/quotes/Quote.js
@@ -1,13 +1,20 @@
-import { selectQuote } from "./quoteSlice"
-import { useSelector } from "react-redux"
+import React, { useEffect } from "react"
+import { selectQuote, selectAuthor, getQuote } from "./quoteSlice"
+import { useSelector, useDispatch } from "react-redux"
 
 export const Quote = () => {
-	const quote = useSelector(selectQuote)
+	const quote = useSelector(selectQuote);
+	const author = useSelector(selectAuthor);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getQuote())
+	}, [dispatch])
 
 	return  (
 		<div className="quote-container">
-			<p className="quote">{quote.quote}</p>
-			<p className="author">{quote.author}</p>
+			<p className="quote">{quote}</p>
+			<p className="author">{author}</p>
 		</div>
 	)
 		

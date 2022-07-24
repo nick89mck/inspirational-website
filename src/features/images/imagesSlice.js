e70f06fc84d47a98b6ea14766905ba421eabe98f
Nick McKinnon <nick89.mck@gmail.com>
2022-07-24 14:30:00 -0400

    Adds thunk and reducers to switch between images

--- src/features/images/imagesSlice.js
+++ src/features/images/imagesSlice.js
@@ -1,18 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import unsplashApi from "../../api/unsplash";
+
+export const getBackgroundImage = createAsyncThunk(
+	'images/getImage',
+	unsplashApi.getImage
+)
 
 const imagesSlice = createSlice(
 	{
 		name: 'images',
 		initialState: {
-			activeImage: {},
+			activeImageIndex: 0,
 			allImages : []
 		},
 		reducers: {
-			getAllImages: (state, action) => {
-				state.allImages = action.payload;
+			switchToNextImage: (state) => {
+				state.activeImageIndex = (state.activeImageIndex + 1) % state.allImages.length;
 			},
-			setActiveImage: (state, action) => {
-				state.activeImage = action.payload
+			switchToPreviousImage: (state) => {
+				let newIndex = state.activeImageIndex - 1;
+				if (newIndex < 0) {
+					newIndex = state.allImages.length - 1;
+				}
+
+				state.activeImageIndex = newIndex;
+			}
+		},
+		extraReducers: {
+			[getBackgroundImage.fulfilled]: (state, action) => {
+				state.allImages = action.payload;
+				state.activeImageIndex = 0;
 			}
 		}
 	}
@@ -20,6 +37,6 @@ const imagesSlice = createSlice(
 
 export const selectActiveImage = (state) => state.images.activeImage;
 export const selectAllImages = (state) => state.images.allImages
-export const { getAllImages, setActiveImage } = imagesSlice.actions
+export const { switchToNextImage, switchToPreviousImage } = imagesSlice.actions
 
 export default imagesSlice.reducer;

6f6a6bc941d30707d22ce65604d6e89c20dc86a5
Nick McKinnon <nick89.mck@gmail.com>
2022-07-24 16:12:59 -0400

    forces images grabbed from api to be landscape

--- src/api/unsplash/index.js
+++ src/api/unsplash/index.js
@@ -1,4 +1,4 @@
-const API_URL = `https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=${process.env.REACT_APP_unsplashAccess}`;
+const API_URL = `https://api.unsplash.com/search/photos?query=nature&orientation=landscape&page=1&per_page=5&client_id=${process.env.REACT_APP_unsplashAccess}`;
 
 export const getImage = async () => {
 	const response = await fetch(API_URL);
